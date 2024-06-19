35df97602446d9d2ef53c5e1339c0f8e54b6767a
Marco Alonso <marcoalonsorota@hotmail.com>
2024-06-19 08:32:25 -0600

    Initial Commit

---
+++ NetworkMonitorSwiftUI/NetworkMonitorSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkMonitorSwiftUIApp.swift
+//  NetworkMonitorSwiftUI
+//
+//  Created by Marco Alonso on 19/06/24.
+//
+
+import SwiftUI
+
+@main
+struct NetworkMonitorSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
